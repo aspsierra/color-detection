4ba7231fea46f202919054784cf169d8a7b5a0f4
Álvaro Sierra <asp.sierra@gmail.com>
2024-12-05 11:51:52 +0100

    Refactored code, created main function

--- color-detection.py
+++ color-detection.py
@@ -7,32 +7,8 @@ from tkinter import filedialog
 root = tk.Tk()
 root.withdraw()
 
-img_path = filedialog.askopenfilename(
-    title="Select an Image",
-    filetypes=[("Image Files", "*.jpg *.jpeg *.png *.bmp *.tiff"), ("All Files", "*.*")]
-)
-
-if not img_path:
-    print("No file selected. Exiting...")
-    exit()
-
-# Load the image
-img = cv2.imread(img_path)
-if img is None:
-    print("Failed to load the image. Please check the file.")
-    exit()
-
-index = ["color", "color_name", "hex", "R", "G", "B"]
-csv = pd.read_csv('./data/colors_dataset.csv', names=index, header=None)
-
-
-
-clicked = False
-r = g = b = xpos = ypos = 0
-
 def draw(event, x, y, flag, params):
     if event == cv2.EVENT_LBUTTONDBLCLK:
-        global b, g, r, xpos, ypos, clicked
         clicked = True
         xpos = x
         ypos = y
@@ -51,33 +27,55 @@ def get_color_name(R, G, B):
     return cname
 
 
-cv2.namedWindow('image')
-cv2.setMouseCallback('image', draw)
+def main():
+    global b, g, r, xpos, ypos, clicked
+    index = ["color", "color_name", "hex", "R", "G", "B"]
+    csv = pd.read_csv('./data/colors_dataset.csv', names=index, header=None)
+
+    img_path = filedialog.askopenfilename(
+        title="Select an Image",
+        filetypes=[("Image Files", "*.jpg *.jpeg *.png *.bmp *.tiff"), ("All Files", "*.*")]
+    )
+
+    if not img_path:
+        print("No file selected. Exiting...")
+        exit()
+
+    img = cv2.imread(img_path)
+    if img is None:
+        print("Failed to load the image. Please check the file.")
+        exit()
+
+    cv2.namedWindow('image')
+    cv2.setMouseCallback('image', draw)
 
 
-while True:
+    while True:
 
-    cv2.imshow("image", img)
-    cv2.waitKey(20)
-    if (clicked):
+        cv2.imshow("image", img)
+        cv2.waitKey(20)
+        if (clicked):
 
-        #cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
-        cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
+            #cv2.rectangle(image, startpoint, endpoint, color, thickness)-1 fills entire rectangle 
+            cv2.rectangle(img,(20,20), (750,60), (b,g,r), -1)
 
-        #Creating text string to display( Color name and RGB values )
-        text = get_color_name(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
-        
-        #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
-        cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
-
-        #For very light colours we will display text in black colour
-        if(r+g+b>=600):
-            cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
+            #Creating text string to display( Color name and RGB values )
+            text = get_color_name(r,g,b) + ' R='+ str(r) +  ' G='+ str(g) +  ' B='+ str(b)
             
-        clicked=False
+            #cv2.putText(img,text,start,font(0-7),fontScale,color,thickness,lineType )
+            cv2.putText(img, text,(50,50),2,0.8,(255,255,255),2,cv2.LINE_AA)
 
-    #Break the loop when user hits 'esc' key    
-    if cv2.waitKey(20) & 0xFF ==27:
-        break
+            #For very light colours we will display text in black colour
+            if(r+g+b>=600):
+                cv2.putText(img, text,(50,50),2,0.8,(0,0,0),2,cv2.LINE_AA)
+                
+            clicked=False
+
+        #Break the loop when user hits 'esc' key    
+        if cv2.waitKey(20) & 0xFF ==27:
+            break
   
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
